c2e867e58ae79bb610472f4fd9f9b98e0057e402
abj90 <abrahamcastrojc@gmail.com>
2022-02-01 23:28:28 +0100

    feat: detail character page updated

--- src/app/components/detail-character-page/detail-character-page.component.ts
+++ src/app/components/detail-character-page/detail-character-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import { getCharacterData } from '@actions/character.actions';
+import { AppState } from '@state/app.state';
+import { selectCharacter } from '@selectors/characters.selector';
+import { ICharacter } from '@models/interfaces';
 
 @Component({
   selector: 'app-detail-character-page',
   templateUrl: './detail-character-page.component.html',
-  styleUrls: ['./detail-character-page.component.sass']
+  styleUrls: ['./detail-character-page.component.sass'],
 })
 export class DetailCharacterPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.store.dispatch(getCharacterData({ id: params.get('id') }));
+    });
   }
 
+  onBack(): void {
+    this.router.navigate(['characters']);
+  }
 }
