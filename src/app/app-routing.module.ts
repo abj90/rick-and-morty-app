4ffa9202c042c52d9d15f35e2d216bbd466f2ea7
abj90 <abrahamcastrojc@gmail.com>
2022-02-01 23:18:05 +0100

    feat: routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ContainerComponent } from './components/container/container.component';
+import { DetailCharacterPageComponent } from './components/detail-character-page/detail-character-page.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'main', component: ContainerComponent },
-  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'characters', component: ContainerComponent },
+  { path: 'character/:id', component: DetailCharacterPageComponent },
+  { path: '', redirectTo: '/characters', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
