fb1fb70d8c0b025fc58383b14bc6575073edad11
abj90 <abrahamcastrojc@gmail.com>
2022-02-01 23:21:42 +0100

    feat: search input created

--- src/app/components/search-characters/search-characters.component.ts
+++ src/app/components/search-characters/search-characters.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-characters',
   templateUrl: './search-characters.component.html',
-  styleUrls: ['./search-characters.component.sass']
+  styleUrls: ['./search-characters.component.sass'],
 })
-export class SearchCharactersComponent implements OnInit {
+export class SearchCharactersComponent {
+  @Output() refreshData = new EventEmitter<string>();
+  public query: string = '';
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onKey(value: string): void {
+    this.query = value;
   }
 
+  onSearch(): void {
+    this.refreshData.emit(this.query);
+  }
 }
