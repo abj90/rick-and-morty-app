dcfb6e9a0ddaae1fc3ff40a607454e57ae0ffa4e
abj90 <abrahamcastrojc@gmail.com>
2022-02-03 00:20:39 +0100

    feat: share function and const file created

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,7 @@
+export const ID = 'id';
+export const CHARACTERS = 'characters';
+export const QUERY = 'query';
+export const STATUS_ICON = 'status-icon ';
+export const ALIVE = 'Alive';
+export const CHARACTER_ALIVE = 'character-alive';
+export const CHARACTER_DEAD = 'character-dead';
